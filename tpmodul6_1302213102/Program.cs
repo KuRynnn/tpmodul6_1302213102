7e4b3187a11d2577e560b4b0bfbbbc3be0762c15
KuRynnn <fahryan107@gmail.com>
2023-03-26 17:38:38 +0700

    implementasi design by contract

--- tpmodul6_1302213102/Program.cs
+++ tpmodul6_1302213102/Program.cs
@@ -4,11 +4,30 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design by Contract - Fahryan Anggriantaka");
-        Console.WriteLine("Video ditonton sekali");
-        video1.IncreasePlayCount(1);
-        Console.WriteLine("Video ditonton dua kali");
-        video1.IncreasePlayCount(1);
-        video1.PrintVideoDetails();
+        try
+        {
+            SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design by Contract - Fahryan Anggriantaka");
+            Console.WriteLine("Video ditonton sekali");
+            video1.IncreasePlayCount(1);
+            Console.WriteLine("Video ditonton dua kali");
+            video1.IncreasePlayCount(1);
+            video1.PrintVideoDetails();
+            Console.WriteLine();
+
+            Console.WriteLine("Video ditonton nyaris limit int");
+            for (int i = 0; i < 214; i++)
+            {
+                video1.IncreasePlayCount(10000000);
+            }
+            video1.PrintVideoDetails();
+            Console.WriteLine(); 
+
+            Console.WriteLine("COBA DI OVRFLOWIN DENGAN DITAMBAH 10.000.000 lagi");
+            video1.IncreasePlayCount(10000000);
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Overflow nih");
+        }
     }
 }
--- tpmodul6_1302213102/SayaTubeVideo.cs
+++ tpmodul6_1302213102/SayaTubeVideo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,15 +16,18 @@ namespace tpmodul6_1302213102
         Random rnd = new Random();
 
         public SayaTubeVideo(String judul)
-        { 
+        {
+            Debug.Assert(judul != null &&  judul.Length <= 100, "Judul tidak sesuai syarat");
             title = judul;
             id = rnd.Next(00000,99999);
             playCount = 0;
         }
         public void IncreasePlayCount(int playCount)
         {
-            this.playCount += playCount;
+            Debug.Assert(playCount <= 10000000, "Jumlah penayangan tidak sesuai syarat");
+            this.playCount = checked(this.playCount + playCount);
         }
+
         public void PrintVideoDetails()
         {
             Console.WriteLine("ID Film = " + id);

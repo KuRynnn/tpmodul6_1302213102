d72d8a514dc3b6c43105e89df8193ddd001c2ccc
KuRynnn <fahryan107@gmail.com>
2023-03-26 16:34:48 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302213102/Program.cs
+++ tpmodul6_1302213102/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302213102;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design by Contract - Fahryan Anggriantaka");
+        Console.WriteLine("Video ditonton sekali");
+        video1.IncreasePlayCount(1);
+        Console.WriteLine("Video ditonton dua kali");
+        video1.IncreasePlayCount(1);
+        video1.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_1302213102/SayaTubeVideo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302213102
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        Random rnd = new Random();
+
+        public SayaTubeVideo(String judul)
+        { 
+            title = judul;
+            id = rnd.Next(00000,99999);
+            playCount = 0;
+        }
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID Film = " + id);
+            Console.WriteLine("Judul FIlm = " + title);
+            Console.WriteLine("Jumlah diputar = " + playCount);
+        }
+    }
+}
